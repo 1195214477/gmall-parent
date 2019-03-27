088bf2c56b85c3e8ac91e9e06e0771a61bfc1ad8
zhangyan <1195214477@qq.com>
2019-03-27 08:41:21 +0800

    Initial commit

---
+++ gmall-api/src/main/java/com/atguigu/gmall/pms/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.atguigu.gmall.pms.service;
+
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.to.PmsProductParam;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * 商品信息 服务类
+ * </p>
+ *
+ * @author Lfy
+ * @since 2019-03-19
+ */
+public interface ProductService extends IService<Product> {
+
+    //分页查询出来的数据
+    Map<String, Object> pageProduct(Integer pageSize, Integer pageNum);
+
+    /**
+     * 给数据库保存商品信息
+     * @param productParam
+     */
+    void create(PmsProductParam productParam);
+
+//    public Long saveProduct(PmsProductParam productParam);
+//    public void saveOther(PmsProductParam productParam);
+}
